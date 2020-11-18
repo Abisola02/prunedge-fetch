df9b2528d4fa23cea21d28f53432d93184a805be
abisola02 <sotejubisola@gmail.com>
2020-11-18 21:29:25 +0100

    i added style

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// function newsApi() {
-//     fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&page=1')
-//     .then((res) => {return res.json()})
-//     .then((data) => {
-//         results = data.articles
-//         console.log(results)
-//     })
-
-// }
-// newsApi()
-
 
 let next = document.querySelector('.next');
 next.addEventListener('click',nextButton);
@@ -17,24 +6,24 @@ next.addEventListener('click',nextButton);
 let count = 0
 function nextButton() {
    count++ 
-   console.log(count); 
-//    let increase = `page=${count}`
+   //console.log(count); 
+   let increase = `page=${count}`
    //console.log(increase);
 
-    // let heading = `<h1 class= "text-center display-4 jumbotron"> IROYIN Page ${count}`
-    // fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&${increase}`)
-    // .then((res) => {return res.json()})
-    // .then((data) => {
-    //     results = data.articles
-    //     let output ="";
-    //     //console.log(results)
-    //     results.forEach(result =>{
-    //     output += `<li class="list-group-item m-4"> Title: ${result.title}  <br><br> Description: ${result.description} </li>`            
-    //     })
+    let heading = `<h1 class= "text-center display-4 jumbotron"> IROYIN Page ${count}`
+    fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&${increase}`)
+    .then((res) => {return res.json()})
+    .then((data) => {
+        results = data.articles
+        let output ="";
+        //console.log(results)
+        results.forEach(result =>{
+        output += `<li class="list-group-item m-4"> Title: ${result.title}  <br><br> Description: ${result.description} </li>`            
+        })
 
-    //     document.querySelector('.newsFeed').innerHTML = output
-    //     document.querySelector('.heading').innerHTML = heading
-    // })  
+        document.querySelector('.newsFeed').innerHTML = output
+        document.querySelector('.heading').innerHTML = heading
+    })  
 }
 nextButton()
 
@@ -49,23 +38,23 @@ prev.addEventListener('click',prevButton);
 
 function prevButton() {
     count--
-    console.log(count);
+    //console.log(count);
     if (count > 1){
-    // let increase = `page=${count}`
-    // let heading = `<h1 class= "text-center display-4 jumbotron"> IROYIN Page ${count}`
-    // fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&${increase}`)
-    // .then((res) => {return res.json()})
-    // .then((data) => {
-    //     results = data.articles
-    //     let output ="";
-    //     //console.log(results)
-    //     results.forEach(result =>{
-    //     output += `<li class="list-group-item m-4"> Title: ${result.title}  <br><br> Description: ${result.description} </li>`            
-    //     })
+    let increase = `page=${count}`
+    let heading = `<h1 class= "text-center display-4 jumbotron"> IROYIN Page ${count}`
+    fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&${increase}`)
+    .then((res) => {return res.json()})
+    .then((data) => {
+        results = data.articles
+        let output ="";
+        //console.log(results)
+        results.forEach(result =>{
+        output += `<li class="list-group-item m-4"> Title: ${result.title}  <br><br> Description: ${result.description} </li>`            
+        })
 
-    //     document.querySelector('.newsFeed').innerHTML = output
-    //     document.querySelector('.heading').innerHTML = heading
-    // }) 
+        document.querySelector('.newsFeed').innerHTML = output
+        document.querySelector('.heading').innerHTML = heading
+    }) 
     
     } else {
 
@@ -74,11 +63,3 @@ function prevButton() {
 
 }    
 prevButton();
-
-// function newsApi() {
-//     fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&page=1')
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-// }
-
-// newsApi()
